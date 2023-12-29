ec6bf36c799eccd2390b357b2fbd5f3965f1cbdb
ruldiaz <rulhdiaz@gmail.com>
2023-12-28 20:33:22 -0600

    commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 console.log("Hello World!");
-const topMenuItem = document.querySelector(".top-menu a");
-console.log(topMenuItem);
+
+function getDate(){
+   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+   
+   return monthNames[(new Date().getMonth())] + " " + new Date().getDate() + ", " + new Date().getFullYear() + ", " + new Date().getHours() + ":" + new Date().getMinutes();
+}
+
+const todaysDate = document.querySelector(".date");
+console.log(todaysDate);
+todaysDate.insertAdjacentText("beforeend", getDate());
